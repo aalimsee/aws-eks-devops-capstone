666b2254cb7bebd55462f83403b6fcf6f897d85e
aaron lim <aaron.limse@hotmail.com>
2025-04-16 12:15:45 +0800

    add dev tf deploy into yml

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -5,14 +5,14 @@ module "eks_cluster" {
   cluster_name    = "dev-eks-cluster"
   cluster_version = "1.29"
   subnet_ids      = data.aws_subnets.filtered_subnets.ids
-  vpc_id          = data.aws_vpcs.filtered_vpcs
+  vpc_id          = data.aws_vpcs.filtered_vpcs.id
 
   eks_managed_node_groups = {
     dev-workers = {
       desired_size   = 2
       max_size       = 3
       min_size       = 1
-      instance_types = ["t3.micro"]
+      instance_types = ["t2.micro"]
     }
   }
 }
