c5996a8f21211ca5dcb456d3ae09d917d0c78edb
aaron lim <aaron.limse@hotmail.com>
2025-04-16 15:58:43 +0800

    pass between github action and eks deployment.yml

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -27,11 +27,18 @@ terraform {
 # Terraform code for dev EKS cluster
 
 module "eks_cluster" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0" # Make sure to pin this or latest stable
+
   cluster_name    = "dev-eks-cluster"
   cluster_version = "1.29"
-  subnet_ids      = data.aws_subnets.public.ids
-  vpc_id          = data.aws_vpcs.filtered_vpcs.ids[0]
+
+  vpc_id     = data.aws_vpcs.filtered_vpcs.ids[0]
+  subnet_ids = data.aws_subnets.public.ids
+
+  enable_irsa = true # Enables IAM Roles for Service Accounts
+  //manage_aws_auth                = true # Make the module handle aws-auth ConfigMap
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     dev-workers = {
@@ -39,6 +46,11 @@ module "eks_cluster" {
       max_size       = 3
       min_size       = 1
       instance_types = ["t3.medium"]
+
+      # Optional, but highly recommended
+      iam_role_additional_policies = {
+        AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+      }
     }
   }
 }
