fb5b86ba963b2d0cc6158c319abe2d1538997131
aaron lim <aaron.limse@hotmail.com>
2025-04-16 15:03:04 +0800

    t2 to t3.medium for eks

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -38,7 +38,7 @@ module "eks_cluster" {
       desired_size   = 2
       max_size       = 3
       min_size       = 1
-      instance_types = ["t2.micro"]
+      instance_types = ["t3.medium"]
     }
   }
 }
